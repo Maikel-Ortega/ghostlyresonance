5824e668ce89ef348b0688680ef24533e052d3d8
Maikel <maikel.or.hi@gmail.com>
2017-01-21 17:48:33 +0100

    Me chupa un webo

---
+++ Assets/Source/DialogInteractuable.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogInteractuable : MonoBehaviour 
+{
+	public void StartDialog(string dialogKey)
+	{
+		
+	}
+}
---
+++ Assets/Source/InteractuableItem.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractuableItem : MonoBehaviour 
+{
+	public delegate void InteractuableEvent(InteractuableItem i);
+	public event InteractuableEvent OnInteract;
+	public bool interacting = false;
+	public bool canInteract = true;
+
+	public virtual void Interact()
+	{
+		if(canInteract)
+		{
+			interacting = !interacting;
+		}
+	}
+}
