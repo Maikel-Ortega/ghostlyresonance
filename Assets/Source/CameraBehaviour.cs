6e7fb406297421a87fa535208f95f29f23a8632f
Lucas <lucasgonzalezp@gmail.com>
2017-01-21 21:10:06 +0100

    Dotween Upgrade

--- Assets/Source/CameraBehaviour.cs
+++ Assets/Source/CameraBehaviour.cs
@@ -1,26 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using DG.Tweening;
 
 [UnitySingleton(UnitySingletonAttribute.Type.ExistsInScene, true)]
-public class CameraBehaviour : UnitySingleton<CameraBehaviour> {
+public class CameraBehaviour : UnitySingleton<CameraBehaviour>
+{
 
     // Use this for initialization
 
     public delegate void OnFocusFinishedDelegate(Transform transform);
     public OnFocusFinishedDelegate OnFocusFinished;
+    public Transform playerTransform;
 
 
     Transform objectFocused;
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    bool followingPlayer = true;
+
+
+    void FixedUpdate()
+    {
+        if (followingPlayer)
+        {
+            Vector3 newTransform = new Vector3(playerTransform.position.x, Camera.main.transform.position.y, Camera.main.transform.position.z);
+            Camera.main.transform.position = newTransform;
+        }
+        
+    }
 
     public void FocusOnTransform(Transform _objectFocused)
     {
         this.objectFocused = _objectFocused;
+        Vector3 v3NewCameraPosition = new Vector3(_objectFocused.position.x, Camera.main.transform.position.y, Camera.main.transform.position.z);
+        followingPlayer = false;
+        Camera.main.transform.DOMove(v3NewCameraPosition, 2f).SetEase(Ease.Linear).OnComplete(() =>
+        {
+            followingPlayer = true;
+            OnFocusFinished(_objectFocused);
+
+        });
+
     }
 }
