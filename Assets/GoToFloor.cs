ce291982f5c95209d9b63058d3e62d3f9816303c
Lucas <lucasgonzalezp@gmail.com>
2017-01-21 23:16:38 +0100

    Mejor camera movement

---
+++ Assets/GoToFloor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoToFloor : MonoBehaviour {
+
+    public Transform t;
+    public float yDestino;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.U)){
+            CameraBehaviour.Instance.FocusOnTransform(t, yDestino);
+        }
+    }
+
+}
