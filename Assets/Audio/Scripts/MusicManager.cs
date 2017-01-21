f96f0b360e41f6e2cf6c6899abf676c097afaeff
parejomusic <parejomusic@gmail.com>
2017-01-21 20:25:07 +0100

    Import AudioPackage

---
+++ Assets/Audio/Scripts/MusicManager.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class MusicManager : MonoBehaviour {
+
+	public AudioSource [] ghost;
+	public AudioSource Ambient, music1, music2,music3;
+	public AudioSource cbass,violins,pedvi;
+	public int  time = 20;
+	public float timecont=0;
+	public int instruments= 2;
+	public bool control=true;
+
+
+	void PlayGhost(int id){
+	
+		ghost [id].Play();
+
+	}
+
+	void bajarvolumen(AudioSource audio){
+		float time = 0;
+	
+			
+	}
+
+
+	void MusicSystem(){
+
+		int random;
+		random = Random.Range (0, instruments);
+
+		switch(random) //donde opción es la variable a comparar
+		{
+		case 0: 
+
+			if (!violins.isPlaying) 
+			{
+			   
+				violins.Play ();
+				violins.DOFade (1f, 2f);
+			
+			} 
+			else 
+			{
+				violins.DOFade (0f, 2f).OnComplete (() => {
+					violins.Stop ();
+				}
+				);
+
+			}
+
+
+			break;
+		
+		case 1: 
+
+			if (!pedvi.isPlaying) 
+			{
+
+				pedvi.Play ();
+				pedvi.DOFade (1f, 2f);
+
+			} 
+			else 
+			{
+
+				pedvi.DOFade (0f, 2f).OnComplete (() => {
+					pedvi.Stop ();
+				}
+				);
+
+
+			}
+
+			break;
+	    }
+
+	}
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		timecont += 1 *Time.deltaTime ;
+
+		if (timecont >= 5 && timecont<6 && control) {
+		
+			MusicSystem ();
+			control = false;
+
+		} 
+
+
+		if (timecont > 6) 
+		
+		{
+			timecont = 0;
+			control = true;
+		}
+
+
+	}
+
+}
---
+++ Assets/Audio/Scripts/SfxManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SfxManager : MonoBehaviour {
+
+	AudioSource [] listSfx;
+	AudioSource sound;
+
+	void PlayAnySound(){
+	
+		if (sound != null) 
+		{
+			sound.Play ();
+		} 
+		else 
+		{
+			Debug.Log(sound.name + " is null");
+		}
+	
+	}
+
+	void PlayAnyRandomSound(){
+	
+		int random;
+
+		if(listSfx!=null){
+			
+		random = Random.Range (0, listSfx.Length);
+
+			listSfx [random].Play ();
+		}
+		else
+		{
+			Debug.Log ( "list is null");
+		}
+	}
+
+	void RandomPitch(){
+
+		float randompn,randomvalue;
+
+		randompn = Random.Range (0, 1);
+		randomvalue = Random.Range (0.0f, 0.9f);
+
+		if (randompn == 1) {
+			randomvalue=randomvalue*(-1);
+		}
+	
+		sound.pitch += randomvalue;
+	
+	
+	}
+		
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		
+	}
+}
