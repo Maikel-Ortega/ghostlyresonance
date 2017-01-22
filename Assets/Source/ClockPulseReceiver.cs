456e3775df3a00b9d09aa11ccd47aeb0603a9829
Maikel <maikel.or.hi@gmail.com>
2017-01-22 13:17:32 +0100

    Main

---
+++ Assets/Source/ClockPulseReceiver.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClockPulseReceiver : PulseReceiver 
+{
+	public HOURS currentHour;
+
+	protected override void Reaction ()
+	{
+		base.Reaction ();
+
+		switch(currentHour)
+		{
+			case HOURS.THREE: 
+				currentHour =HOURS.SIX;
+				break;
+			case HOURS.SIX: 
+				currentHour = HOURS.NINE;
+				break;
+			case HOURS.NINE: 
+				currentHour = HOURS.TWELVE;
+				break;
+			case HOURS.TWELVE: 
+				currentHour = HOURS.THREE;
+			break;
+		}
+
+		GameObject.FindObjectOfType<HotelManager>().SetGhostsConfigByHour(currentHour);
+
+	}
+}
+
