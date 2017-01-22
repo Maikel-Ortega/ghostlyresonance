1450267c8c9c23d4acb834acb1ecf61d2b4574f0
Lucas <lucasgonzalezp@gmail.com>
2017-01-22 13:37:15 +0100

    Fantasmas con sonido y tal

---
+++ Assets/PlayOnAwakeAndFade.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+public class PlayOnAwakeAndFade : MonoBehaviour {
+
+	// Use this for initialization
+	void Awake () {
+        AudioSource sour = GetComponent<AudioSource>() ;
+        sour.volume = 0f;
+        sour.Play();
+        sour.DOFade(1f, 2f);
+	}
+	
+}
